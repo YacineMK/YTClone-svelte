5dada3e074ec054c74850c2ec979445bff327259
YacineMK <merroukiyacine06@gmail.com>
2024-01-28 08:20:28 +0100

    ressponsive head 2.0

--- src/lib/components/Head.svelte
+++ src/lib/components/Head.svelte
@@ -2,8 +2,9 @@
     import Searchbar from "./Searchbar.svelte";
 </script>
 
-<nav class="top-0 left-0 flex justify-between h-16 items-center w-auto px-4 py-2 bg-white">
+<nav class="top-0 left-0 flex justify-between h-16 items-center w-auto px-4 py-2 border-b-gray-800 bg-white">
     <img src="ytLogo.svg" alt="logo"/>
     <Searchbar />
-    <img src="avatar.png" alt="avatar" class="w-12 h-12 rounded-full sm:w-0"/>
+    <img src="avatar.png" alt="avatar" class="h-12 rounded-full w-10 "/>
 </nav> 
+
--- src/lib/components/Searchbar.svelte
+++ src/lib/components/Searchbar.svelte
@@ -2,7 +2,7 @@
     import {SearchOutline} from 'flowbite-svelte-icons';
 </script>
 
-<form method="post" class="h-5 flex w-[390px] px-3 items-center">
+<form method="post" class="h-5 flex max-w-[400px] px-3 items-center">
     <input type="text" placeholder="search" class="border-2 h-8 placeholder-opacity-30 flex w-[80%] px-3 rounded-l-xl outline-none border-opacity-50 border-gray-300"/>
     <button class="border-2 border-l-0 border-gray-300 border-opacity-50 py-1 pr-1 rounded-r-xl">
         <SearchOutline class="px-1 opacity-50"/>
