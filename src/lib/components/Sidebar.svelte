e366565b44eb0468e868582eb84fa1cd2634e497
YacineMK <merroukiyacine06@gmail.com>
2024-01-29 14:51:03 +0100

    f

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="bg-white hidden flex-col md:flex pt-7 fixed items-center pl-12 pr-14 top-[63px] h-screen border-t border-r border-gray-400 border-opacity-50">
-        <a href="/" class="flex justify-center items-center py-5"><HomeOutline /> <p class="text-s ml-2 capitalize">{nav[0]}</p></a>
-        <a href="/about" class="flex justify-center items-center py-5"><FireOutline /> <p class="text-s ml-2 capitalize">{nav[1]}</p></a> 
-        <a href="/" class="flex justify-center items-center py-5"><GithubSolid /> <p class="text-s ml-2 capitalize">{nav[2]}</p></a>
+        <a href="/" class="flex justify-center items-center px-2 py-5"><HomeOutline /> <p class="text-s ml-2 capitalize">{nav[0]}</p></a>
+        <a href="/about" class="flex justify-center items-center px-2 py-5"><FireOutline /> <p class="text-s ml-2 capitalize">{nav[1]}</p></a> 
+        <a href="/" class="flex justify-center items-center px-2 py-5"><GithubSolid /> <p class="text-s ml-2 capitalize">{nav[2]}</p></a>
 </div>
